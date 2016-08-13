a13882e4c6d60c9d0a576faab8c94016bfe9be73
Bob Maerten <bob@levups.com>
2016-08-13 22:07:29 +0200

    Apply Rubocop recommendations throughout codebase
    Also remove unnecessary ending lines ';'

--- lib/inky/rails/template_handler.rb
+++ lib/inky/rails/template_handler.rb
@@ -1,13 +1,15 @@
 module Inky
   module Rails
     class TemplateHandler
-      def self.erb_handler
-        @@erb_handler ||= ActionView::Template.registered_template_handler(:erb)
-      end
+      class << self
+        def erb_handler
+          @erb_handler ||= ActionView::Template.registered_template_handler(:erb)
+        end
 
-      def self.call(template)
-        compiled_source = erb_handler.call(template)
-        "Inky::Core.new.release_the_kraken(begin; #{compiled_source};end)"
+        def call(template)
+          compiled_source = erb_handler.call(template)
+          "Inky::Core.new.release_the_kraken(begin; #{compiled_source};end)"
+        end
       end
     end
   end
