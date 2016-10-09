eae332d0ded19f71a464d526fc1fa351fffd60b2
Marc-Andre Lafortune <github@marc-andre.ca>
2016-10-08 22:23:52 -0400

    More cases, with one extra failure

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,7 @@ def reformat_html(html)
     .gsub(%r{<(\w+)([^>]*)>\n</\1>}, '<\1\2/>') # Auto close empty tags, e.g. <hr>\n</hr> => <hr/>
     .gsub(/ "/, '"').gsub(/\=" /, '="')         # Remove leading/trailing spaces inside attributes
     .gsub(/ </, '<').gsub(/> /, '>')            # Remove leading/trailing spaces inside tags
+    .gsub(' data-parsed=""', '')                # Don't consider this known inky-node artefact
     .gsub(/class\="([^"]+)"/) do                # Sort class names
       classes = $1.split(' ').sort.join(' ')
       %{class="#{classes}"}
