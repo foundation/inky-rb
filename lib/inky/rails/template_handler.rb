155ab9f7cf91130b8ced1ef721890340f60175ee
Marc-Andre Lafortune <github@marc-andre.ca>
2016-10-18 00:23:51 -0400

    Refactor to use class instead of singleton

--- lib/inky/rails/template_handler.rb
+++ lib/inky/rails/template_handler.rb
@@ -1,22 +1,25 @@
 module Inky
   module Rails
     class TemplateHandler
-      class << self
-        def engine_handler
-          type = ::Inky.configuration.template_engine
-          ActionView::Template.registered_template_handler(type) ||
-            raise("No template handler found for #{type}")
-        end
+      def initialize(compose_with = nil)
+        @engine_handler = ActionView::Template.registered_template_handler(compose_with) if compose_with
+      end
 
-        def call(template)
-          compiled_source = engine_handler.call(template)
-          "Inky::Core.new.release_the_kraken(begin; #{compiled_source};end)"
-        end
+      def engine_handler
+        return @engine_handler if @engine_handler
+        type = ::Inky.configuration.template_engine
+        ActionView::Template.registered_template_handler(type) ||
+          raise("No template handler found for #{type}")
+      end
+
+      def call(template)
+        compiled_source = engine_handler.call(template)
+        "Inky::Core.new.release_the_kraken(begin; #{compiled_source};end)"
       end
     end
   end
 end
 
 ActiveSupport.on_load(:action_view) do
-  ActionView::Template.register_template_handler :inky, Inky::Rails::TemplateHandler
+  ActionView::Template.register_template_handler :inky, Inky::Rails::TemplateHandler.new
 end
