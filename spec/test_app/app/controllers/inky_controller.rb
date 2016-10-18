5923301956646553562cbe44627ab4bb98735238
Marc-Andre Lafortune <github@marc-andre.ca>
2016-10-18 00:58:55 -0400

    Refactor

--- spec/test_app/app/controllers/inky_controller.rb
+++ spec/test_app/app/controllers/inky_controller.rb
@@ -1,16 +1,9 @@
 class InkyController < ApplicationController
   layout 'inky_layout', only: [:layout]
 
-  def simple
-  end
-
   def non_inky
     @partial_name = params[:partial].presence
   end
 
-  def layout
-  end
-
-  def slim
-  end
+  %w[simple layout slim].each{ |m| define_method(m){} }
 end
