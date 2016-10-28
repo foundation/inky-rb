37b90c4a80ab36c999c6a3b726f087b5f0c210b0
Zoran <zoran@zurb.com>
2016-10-28 15:06:44 -0700

    switch to guard clause

--- lib/generators/inky/install_generator.rb
+++ lib/generators/inky/install_generator.rb
@@ -11,11 +11,10 @@ module Inky
       class_option :slim, desc: "Generate layout in Slim", type: :boolean
 
       def preserve_original_mailer_layout
-        if layout_name == 'mailer' && extension == 'erb'
-          original_mailer = File.join(layouts_base_dir, "mailer.html.erb")
-          rename_filename = File.join(layouts_base_dir, "old_mailer_#{Time.now.to_i}.html.erb")
-          File.rename(original_mailer, rename_filename) if File.exist? original_mailer
-        end
+        return if layout_name == 'mailer' && extension == 'erb'
+        original_mailer = File.join(layouts_base_dir, "mailer.html.erb")
+        rename_filename = File.join(layouts_base_dir, "old_mailer_#{Time.now.to_i}.html.erb")
+        File.rename(original_mailer, rename_filename) if File.exist? original_mailer
       end
 
       def create_mailer_stylesheet
